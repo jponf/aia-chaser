17a2ee72cc2c2d5525ac8dab0ed6e6d669214a9b
Josep Pon Farreny <jponfarreny@gmail.com>
2024-01-23 16:39:26 +0100

    fix: test assert for microsoft.com

--- tests/test_aia_chase.py
+++ tests/test_aia_chase.py
@@ -6,16 +6,18 @@ from aia_chaser import AiaChaser
 @pytest.mark.parametrize(
     "url_string",
     [
-        # "https://www.siemens.com",
+        "https://www.siemens.com",
         "https://www.microsoft.com",
-        # "https://www.amazon.com",
-        # "https://www.google.com",
+        "https://www.amazon.com",
+        "https://www.google.com",
     ],
 )
 def test_aia_chase_url(url_string: str) -> None:
     chaser = AiaChaser()
     chain = chaser.fetch_ca_chain_for_url(url_string=url_string)
-    assert len(chain) > 1
+    # on Windows microsoft.com certificate is trusted resulting
+    # in a chain of length 1 ¯\_(ツ)_/¯
+    assert len(chain) >= 1
 
 
 @pytest.mark.parametrize(
