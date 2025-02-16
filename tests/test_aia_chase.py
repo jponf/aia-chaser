903dfd6073da174643b9bf47a640172b5fd39b8b
Josep Pon Farreny <josep.pon-farreny@siemens.com>
2025-02-17 00:08:39 +0100

    test: Increase number of URLs in test

--- tests/test_aia_chase.py
+++ tests/test_aia_chase.py
@@ -6,11 +6,38 @@ from aia_chaser import AiaChaser
 @pytest.mark.parametrize(
     "url_string",
     [
+        # Companies
+        "https://aliexpress.com",
+        "https://www.baidu.com",
         "https://www.siemens.com",
         "https://www.microsoft.com",
         "https://www.amazon.com",
         "https://www.google.com",
+        # News
+        "https://www.elperiodico.com",
         "https://segre.com",
+        "https://www.nytimes.com",
+        # Governments
+        "https://administracion.gob.es",
+        "https://www.bundesregierung.de",
+        "https://www.elysee.fr",
+        "https://www.gov.uk",
+        "https://www.japan.go.jp",
+        "https://www.usa.gov",
+        # Universities
+        "https://udl.cat",
+        "https://www.upc.edu",
+        "https://www.mit.edu",
+        "https://www.berkeley.edu",
+        "https://en.snu.ac.kr",
+        # NGOs
+        "https://www.redcross.org",
+        "https://www2.cruzroja.es",
+        # Other
+        "https://www.kernel.org",
+        "https://www.fbi.gov",
+        "https://policia.es",
+        "https://mossos.gencat.cat",
     ],
 )
 def test_aia_chase_url(url_string: str) -> None:
@@ -28,6 +55,8 @@ def test_aia_chase_url(url_string: str) -> None:
         "www.microsoft.com",
         "www.amazon.com",
         "www.google.com",
+        "www.segre.com",
+        "www.nytimes.com",
     ],
 )
 def test_aia_chase_url_no_scheme(url_string: str) -> None:
