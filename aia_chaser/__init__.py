a0afbb52e27bd5fa93e17359c2fe70d8e894cd89
Josep Pon Farreny <josep.pon-farreny@siemens.com>
2026-04-26 19:12:49 +0200

    feat: export AiaChaserError and AiaChaserWarning from top-level package

--- aia_chaser/__init__.py
+++ aia_chaser/__init__.py
@@ -1,4 +1,5 @@
 __version__ = "3.4.0"
 
 from .chaser import AiaChaser
+from .exceptions import AiaChaserError, AiaChaserWarning
 from .verify import VerifyCertificatesConfig, verify_certificate_chain
